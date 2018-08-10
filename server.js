b346f72485ce78a3726785c0d61540bd8e843abb
victoriaashling <victoria.ashling@gmail.com>
2018-08-10 10:34:05 -0400

    package.json and simple server logic set up

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.listen(PORT, () => {
+    console.log("Server listening on PORT " + PORT);
+});
